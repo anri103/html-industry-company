012e50d609dd0bdc8952f3346a492e6d903e759a
anri103 <ianrianri@gmail.com>
2024-08-04 19:26:36 +0300

    -

---
+++ app.js
@@ -0,0 +1,46 @@
+// heroSwiper
+const heroSwiper = new Swiper('.heroSwiper', {
+    loop: true,
+    grabCursor: true,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+    // effect: 'fade',
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});
+
+const partnersSwiper = new Swiper('.partnersSwiper', {
+    slidesPerView: 2,
+    spaceBetween: 32,
+    loop: true,
+    grabCursor: true,
+    autoplay: {
+        delay: 3500,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    breakpoints: {
+        768: {
+            slidesPerView: 4,
+            spaceBetween: 40,
+        },
+        1024: {
+            slidesPerView: 5,
+            spaceBetween: 50,
+        },
+    },
+});
+
+// fancybox
+Fancybox.bind("[data-fancybox]", {});
